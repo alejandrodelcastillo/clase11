b5aaf56aac203fde8801a657e41cbb9d265b5b0b
alejandrodelcastillo <130861049+alejandrodelcastillo@users.noreply.github.com>
2023-05-20 03:51:11 -0300

    tarea correspondiente a la clase 11
    entrega de trabajo de la clase 11

---
+++ clase11/js/javascript.js
@@ -0,0 +1,59 @@
+const helado = 150;
+
+let topping = "0";
+let precio;
+let precioFinal;
+
+
+if(topping === "Oreo") {
+    precio = 10;
+
+} else if(topping === "Kitkat") {
+    precio = 15;
+
+}else if(topping === "Kinder") {
+    precio = 25;
+
+} else if(topping === "0") {
+    precio = 0;
+    console.log("no tenemos ese topping");        
+}
+
+precioFinal = precio + helado;
+
+
+console.log("El helado cuesta $" +" " +"precioFinal");
+
+let expr= "menu";
+switch ("carne"){
+    case 'carne':
+        console.log("vino tinto");
+break;
+case'pescado':
+console.log("vino blanco");
+
+break;
+case 'verdura':
+console.log("agua");
+break;
+case 'fuera del menu':
+console.log("elija carne, pescado o verdura");
+}
+
+let count = 11;
+while (i <= 11) {
+    console.log("numero es mas chico que 11");
+}
+
+
+
+
+
+
+
+
+
+
+    
+
+
